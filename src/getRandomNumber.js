83d1cd8597fc3e1bdac21cddf8ed70bb03ccc298
Сергей Прасолов <prasol555555@gmail.com>
2024-04-18 22:47:10 +0300

    debug index.js and getRandomNumber.js

--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -1,6 +1,6 @@
 const getRandomNumber = (min, max) => {
   const minNum = Math.ceil(min);
   const maxNum = Math.floor(max);
-  return Math.floor(Math.random() * (maxNum - minNum) + minNum);
+  return Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
 };
 export default getRandomNumber;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import readlineSync from 'readline-sync';
 
-const engine = (description, generateRound) => {
-  const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+const startGames = (description, generateRound) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(description);
 
-  const countRound = 3;
+  const roundsCount = 3;
 
-  for (let i = 1; i <= countRound; i += 1) {
+  for (let i = 1; i <= roundsCount; i += 1) {
     const [question, correctAnswer] = generateRound();
     console.log(`Question: ${question}`);
 
     const answer = readlineSync.question('Your answer: ');
 
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+    if (answer !== correctAnswer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${name}!`);
 };
-export default engine;
+export default startGames;
